d90ec532084bbb0011141598c2bdff13fc1b4df4
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-04-02 15:53:41 +0900

    이동 점프 동작/애니메이션 구현

---
+++ ShadowDash/Assets/Script/Player.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    private float xInput;
+    private float facingDir= 1f;
+    private bool facingRight = true;
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+
+        Movement();
+        CheckInput();
+        collisionCheck();
+        
+        FlipController();
+        AnimatorControllers();
+
+    }
+
+    private void collisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        Debug.Log(isGrounded);
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        if(isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+
+
+
+
+    //Alt + 화살표키
+    private void AnimatorControllers()
+    {
+
+        bool isMoving = rb.linearVelocity.x != 0;
+        anim.SetBool("Run", isMoving);
+
+    }
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+    private void FlipController()
+    {
+        if (rb.linearVelocityX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.linearVelocityX < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
+}
